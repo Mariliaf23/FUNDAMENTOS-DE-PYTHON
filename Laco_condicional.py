dea4188f9aa3233238367ec65487fe25c390d06e
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-30 20:54:54 -0300

    bbbbb

---
+++ Laco_condicional.py
@@ -0,0 +1,25 @@
+print (True or True) #resultado verdadeiro
+print (True or False) #resultado verdadeiro
+print (False or True) #resultado verdadeiro
+print (False or False) #resultado falso
+
+
+print (True and True) # resultado verdadeiro = apenas se tudo for verdadeiro
+print (False and True) # resultado falso
+print (True and False)  # resultado falso
+print (False and False)  # resultado falso
+
+
+print (not True) # resultado é o contrário da afirmação = False
+print (not False) # resultado é o contrário da afirmação = True
+
+# regra de precedencia 
+# primeiro executa o and, or, not
+
+print(not True)
+print(not False)
+
+print(1==2 and 3<9)
+
+print(1==2 and 3<9 or 5-4 ==1)
+print(1==2 and 3<9 or 3-4 ==1) 
