5b502e05d2c05644fed58a6bf37d2ae635da3d23
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-30 20:34:32 -0300

    atividades teste

---
+++ Atividade_teste.py
@@ -0,0 +1,8 @@
+'''Crie um programa que mostre uma mensagem de boas vindas personalizada.
+“Olá mundo, aqui vamos nós iniciar no universo da programação!”'''
+print("Olá mundo, aqui vamos nós iniciar no universo da programação!")
+
+'''Crie um programa que recebe o nome do usuário e mostre uma mensagem de boas vindas personalizada com o nome do mesmo.
+“Olá {usuário}, seja bem vindo. Aqui vamos iniciar no universo da programação!”'''
+usuário = input("Marília")
+print=("Olá {usuário}, seja bem vindo. Aqui vamos iniciar no universo da programação!")
--- Operacoes-aritmeticas.py
+++ Operacoes-aritmeticas.py
@@ -4,3 +4,6 @@ print("A adição de 5 mais 2 é",(5+2))
 print("A multiplicação de 5 vezes 2 é",(5*2))
 print("A subtração de 5 menos 2 é",(5-2))
 ("A exponenciação de 5 exponencial 2 é",(5**2))
+
+
+
