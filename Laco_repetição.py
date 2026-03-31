5d59d7dd024711ffc93c6e4c5e0b90659bcec9f1
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-31 20:10:58 -0300

    Laço de repetição

---
+++ Laco_repetição.py
@@ -0,0 +1,12 @@
+'''nome = ''
+while nome != "fim":
+    nome = input('Digite seu nome ou "fim" para parar o programa ')
+    print(f'Olá {nome}, seja bem vindo ao laço de repetição')
+print('Saiu do laço de repetição')'''
+
+
+contador = 0
+while contador < 5:
+    print(f'o valor de contador é {contador}, vamos rodar o laço novamente')
+    contador += 1
+print('Saiu do laço de repetição')    
