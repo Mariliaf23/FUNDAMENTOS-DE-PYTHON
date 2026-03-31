590d7f39b3f164d4385ba7fff62cddb29bf668f0
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-03-31 20:54:08 -0300

    Resolve o laço da repetição

--- Laco_repetição.py
+++ Laco_repetição.py
@@ -12,35 +12,6 @@ while contador < 5:
 print('Saiu do laço de repetição')'''    
 
 
-'''secret_number = 777
-
-print(
-"""
-+===================================+
-| Bem vindo ao meu jogo, trouxa!    |
-| Insira um número inteiro          |
-| e adivinhar o número que tenho    |
-| escolhidos para você.             |
-| Então, qual é o número secreto?   |
-+===================================+
-""")
-
-
-# 1. Solicita que o usuário insira o primeiro número
-guess = int(input("Digite o número: ")) #Converte o que o usuário digita em um número inteiro para que possamos compará-lo com 777
-
-# 2. Inicia o loop while
-while guess != secret_number: #Este laço continuará repetindo enquanto o palpite for diferente do número secreto.
-   
-    # 3. Se for diferente, imprime a mensagem de erro e pede novo input
-    print("Ha ha! Você está preso no meu loop!")
-    guess = int(input("Tente novamente: "))
-
-# 4. Se o loop terminou, significa que ele acertou
-print(guess)
-print("Muito bem, trouxa! Você está livre agora.")'''
-
-
 
 secret_number = 777
 
@@ -57,13 +28,17 @@ print(
 """)
 
 # Primeiro input fora do loop
-guess = int(input("Digite o número: "))
+guess = int(input("Digite o número: ")) #Converte o que o usuário digita em um número inteiro para que possamos compará-lo com 777
+
 
 # O loop só entra se o primeiro palpite for errado
-while guess != secret_number:
+
+while guess != secret_number:#Este laço continuará repetindo enquanto o palpite for diferente do número secreto.
     print("Ha ha! Você está preso no meu loop!")
     # Pede um novo número. Se o usuário digitar 777 aqui, 
     # o loop volta ao topo, a condição vira 'False' e ele sai IMEDIATAMENTE.
+
+
     guess = int(input("Tente novamente: "))
 
 # Só executa estas linhas quando o usuário digita 777
