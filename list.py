4f9409f5b8e0d2b00774ea80ace508b4548cd38e
Marilia Ferreira <marilia9643626@edu.pa.senac.br>
2026-04-09 19:05:17 -0300

    Resolve lista

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-lista = [0, 1, 2, 3, 4, 5]
+'''lista = [0, 1, 2, 3, 4, 5]
 var = 1
 print(lista)
 lista[2] = 'dois'
@@ -6,4 +6,13 @@ print(lista)
 
 var_a = 5
 var_b = 1
-var_a = var_b
+var_a = var_b'''
+
+
+
+minha_lista = [5, 6, 7, 8, 9, 10, 11]
+
+minha_lista = [i for i in minha_lista if i % 2 == 0]
+
+print(minha_lista)
+
